62a6407190629a343609101d7d59bc5326895a89
Made Arya <72294087+made69@users.noreply.github.com>
2020-10-03 19:25:04 +0700

    add tiktok downloader

---
+++ tk2.php
@@ -0,0 +1,81 @@
+<?php
+    $tk = $_GET["tk"];
+
+function http_request($url){
+    // persiapkan curl
+    $ch = curl_init(); 
+
+    // set url 
+    curl_setopt($ch, CURLOPT_URL, $url);
+    
+    // set user agent    
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+    // return the transfer as a string 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+    // $output contains the output string 
+    $output = curl_exec($ch); 
+
+    // tutup curl 
+    curl_close($ch);      
+
+    // mengembalikan hasil curl
+    return $output;
+}
+
+$profile = http_request("http://scrap.terhambar.com/tiktokfull?link=$tk");
+
+// ubah string JSON menjadi array
+$profile = json_decode($profile, TRUE);
+?>
+	
+	<?php
+	include("head.php");
+	?>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>TikTok Downloader</h2>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                <hr />
+                <div class="row">
+                <div class="col-md-8">
+				<video width="256" height="144" controls>
+				<source src="<?php echo $profile["result"]["videoData"]["linkVideo"]["withWatermark"] ?>" type="video/mp4">
+				Your browser does not support the video tag.
+				</video>
+				<h5>Username : <?php echo $profile["result"]["username"] ?></h5>
+				<h5>Caption : <?php echo $profile["result"]["caption"] ?></h5>
+				<h5>Lagu : <?php echo $profile["result"]["musicData"]["song"] ?></h5>
+				 <a href="<?php echo $profile["result"]["videoData"]["linkVideo"]["withWatermark"] ?>" class="btn btn-primary">Download Dengan Watermark</a>
+                <a href="<?php echo $profile["result"]["videoData"]["linkVideo"]["withoutWatermark"] ?>" class="btn btn-success">Download Tanpa Watermark</a>
+                </div>
+            </div>
+                </div>
+                <!-- /. ROW  -->
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
