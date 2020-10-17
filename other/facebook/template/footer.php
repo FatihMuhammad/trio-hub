c3b78c74261e364171a27dc0e2f96bdb8dfd445c
Trio Gempar Surya Giri <suryagiri200@gmail.com>
2020-10-18 04:37:06 +0700

    Delete footer.php

--- other/facebook/template/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container text-center">
-        <span class="text-muted">©2017 <?php echo $config["title"]; ?></span>
-    </div>
-</footer>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
-</body>
-</html>
