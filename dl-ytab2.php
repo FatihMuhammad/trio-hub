9957f50fc2e366dccd17007cd71c9b3fa31cde0a
Hilmy <92627742+hilmy69@users.noreply.github.com>
2021-10-16 17:12:42 +0700

    Youtube Audio Downloader V2 - Result

---
+++ dl-ytab2.php
@@ -0,0 +1,84 @@
+<?php
+    $yt = $_GET["yt"];
+	$yta = str_replace('www.youtube.com/watch?v=', 'youtu.be/', $yt);
+	?>
+
+<?php
+
+function http_request($url){
+    // persiapkan curl
+    $ch = curl_init(); 
+
+    // set url 
+    curl_setopt($ch, CURLOPT_URL, $url);
+    
+    // set user agent    
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+    // return the transfer as a string 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+    // $output contains the output string 
+    $output = curl_exec($ch); 
+
+    // tutup curl 
+    curl_close($ch);      
+
+    // mengembalikan hasil curl
+    return $output;
+}
+
+$profile = http_request("https://hadi-api.herokuapp.com/api/yt2/audio?url=$yta");
+
+// ubah string JSON menjadi array
+$profile = json_decode($profile, TRUE);
+?>
+	
+	<?php
+	include("head.php");
+	?>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Youtube Audio Downloader</h2>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                <hr />
+                <div class="row">
+                <div class="col-md-8">
+				Title : <?php echo $profile["result"]["title"] ?>
+							<br /><br />
+							<audio controls style="width:100%"> <source src="<?php echo $profile["result"]["download_audio"] ?>" type="audio/mpeg"> Your browser does not support the audio tag. </audio>
+							<br /><br />
+							<a href="<?php echo $profile["result"]["download_audio"] ?>">
+							<button type="button" class="btn btn-primary">Download</button>
+							</a>
+							<br />
+                </div>
+            </div>
+                </div>
+                <!-- /. ROW  -->
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
