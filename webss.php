614348569866c670dcef8d36674bbd541cc6c432
Tri Suryani <55627125+xtrisur11@users.noreply.github.com>
2021-10-18 02:18:12 +0700

    Webpage Screenshot

---
+++ webss.php
@@ -0,0 +1,48 @@
+
+	
+	<?php
+	include("head.php");
+	?>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Webpage Screenshot</h2>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                <hr />
+                <div class="row">
+						<div class="col-md-7">
+							 <form action="webss2.php" method="GET">
+							 <div class="form-group">
+								<input type="text" class="form-control" name="ss" placeholder="https://www.youtube.com/channel/UCE0TjXblz2AREyaLkMUqLFA" value="">
+							 </div>
+							 <button type="submit" class="btn btn-primary">Submit</button>
+							 </form>
+						</div>
+            </div>
+                </div>
+                <!-- /. ROW  -->
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
