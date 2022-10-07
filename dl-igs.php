1d2f593c888fa106d1510f6a94746c4e3d055c7f
Fatih Maulana Muhammad <59079341+FatihMuhammad@users.noreply.github.com>
2022-10-07 13:55:58 +0700

    Create Instagram Story Downloader Input

---
+++ dl-igs.php
@@ -0,0 +1,57 @@
+<?php
+	include("head.php");
+	?>
+
+<div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+						<div class="card-header card-blue">
+						Instagram Story Downloader
+						</div>	
+                            <div class="card-body">
+                             <form action="dl-igs2.php" method="GET">
+							 <div class="form-group">
+								<input type="text" class="form-control" name="igs" placeholder="suryagiri200" value="">
+							 </div>
+							 <button type="submit" class="btn btn-primary">Submit</button>
+							 </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				
+				<div class="row">
+                    <div class="col-12">
+                        <div class="card">
+						<div class="card-header card-blue">
+						Result
+						</div>	
+                            <div class="card-body">
+							<br /><br /><br />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- Right sidebar -->
+                <!-- ============================================================== -->
+                <!-- .right-sidebar -->
+                <!-- ============================================================== -->
+                <!-- End Right sidebar -->
+                <!-- ============================================================== -->
+            </div>
+			
+<?php
+	include("foot.php");
+	?>
